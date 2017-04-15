bd4b4e1b429273e819130af6668b9319ea152d74
Bryan Tyler Parker <btylerparker@gmail.com>
2017-04-15 16:21:15 -0400

    Scale canvas (#6)
    * Remove console logs
    
    * Removed something from index.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,33 @@
 window.myGame = window.myGame || {};
 
 (function(Phaser, myGame) {
-  var game = new Phaser.Game(256, 240, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+  var game = new Phaser.Game(256, 240, Phaser.CANVAS, '', { init: init, preload: preload, create: create, update: update, render: render });
+  var pixel = { scale: 4, canvas: null, context: null, width: 0, height: 0 }
+
   var tileMapper;
   var cursors;
   var player;
+
+  function init() {
+    //  Hide the un-scaled game canvas
+    game.canvas.style['display'] = 'none';
+    
+    //  Create our scaled canvas. It will be the size of the game * whatever scale value you've set
+    pixel.canvas = Phaser.Canvas.create(game, game.width * pixel.scale, game.height * pixel.scale);
+ 
+    //  Store a reference to the Canvas Context
+    pixel.context = pixel.canvas.getContext('2d');
+ 
+    //  Add the scaled canvas to the DOM
+    Phaser.Canvas.addToDOM(pixel.canvas);
+ 
+    //  Disable smoothing on the scaled canvas
+    Phaser.Canvas.setSmoothingEnabled(pixel.context, false);
+ 
+    //  Cache the width/height to avoid looking it up every render
+    pixel.width = pixel.canvas.width;
+    pixel.height = pixel.canvas.height;
+  }
 
   function preload() {
     // debugger
@@ -14,8 +37,10 @@ window.myGame = window.myGame || {};
   }
 
   function create() {
-    const TileMapper = myGame.TileMapper.TileMapper();
-    TileMapper.bar()
+    tileMapper = myGame.TileMapper.TileMapper();
+    tileMapper.bar()
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     var playerGroup = game.add.group();
     player = new Player(game);
     playerGroup.add(player);
@@ -43,5 +68,7 @@ window.myGame = window.myGame || {};
   }
 
   function render() {
+    //  Every loop we need to render the un-scaled game canvas to the displayed scaled canvas:
+    pixel.context.drawImage(game.canvas, 0, 0, game.width, game.height, 0, 0, pixel.width, pixel.height);
   }
 })(window.Phaser, window.myGame);
