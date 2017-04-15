9919e54873259218ec4238946092ab315808a1ee
Chris Kim <christopherjkim@gmail.com>
2017-04-15 16:16:53 -0400

    add octorok sprite that doesn't do anything

---
+++ src/js/octorok.js
@@ -0,0 +1,16 @@
+window.myGame = window.myGame || {};
+
+(function(Phaser, myGame) {
+  Octorok = function(game) {
+    let x = 120;
+    let y = 120;
+    this.speed = 96;
+    Phaser.Sprite.call(this, game, x, y, 'octorok', 1);
+    game.physics.arcade.enable(this);
+  };
+
+  Octorok.prototype = Object.create(Phaser.Sprite.prototype);
+  Octorok.prototype.constructor = Octorok;
+
+  myGame.Octorok = Octorok;
+})(window.Phaser, window.myGame);
