811b96b8dfedef6108011148ea52e0e1c5f38708
Tyler Parker <btylerparker@gmail.com>
2017-04-15 15:24:26 -0400

    Constructor and spaces

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,22 @@
 window.myGame = window.myGame || {};
 
 (function(Phaser, myGame) {
-    var game = new Phaser.Game(256, 240, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-    var tileMapper;
+  var game = new Phaser.Game(256, 240, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+  var tileMapper;
 
-    function preload() {
-      // debugger
-      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    }
+  function preload() {
+    // debugger
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  }
 
-    function create() {
-      const Foo = myGame.TileMapper.foo();
-      Foo.bar()
-    }
+  function create() {
+    const TileMapper = myGame.TileMapper.TileMapper();
+    TileMapper.bar()
+  }
 
-    function update() {
-    }
+  function update() {
+  }
 
-    function render() {
-    }
+  function render() {
+  }
 })(window.Phaser, window.myGame);
--- src/js/tile-mapper.js
+++ src/js/tile-mapper.js
@@ -1,7 +1,7 @@
 window.myGame = myGame || {}; // Sets myGame to a blank object if it isn't already defined
 
 (function(myGame) {
-  function foo(tileMapperParams) {
+  function TileMapper(tileMapperParams) {
     console.log("constructor Foo");
     // constructor
 
@@ -13,5 +13,5 @@ window.myGame = myGame || {}; // Sets myGame to a blank object if it isn't alrea
     }
   };
 
-  myGame.TileMapper = { foo };
+  myGame.TileMapper = { TileMapper };
 })(window.myGame);
