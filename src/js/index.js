557067425595ac70ed394564722cfba1361c1bbc
Diane Losardo <dlosardo@gmail.com>
2017-04-15 15:30:17 -0400

    player movement updated

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,27 @@ window.myGame = window.myGame || {};
 (function(Phaser, myGame) {
   var game = new Phaser.Game(256, 240, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
   var tileMapper;
+  var cursors;
+  var player;
 
   function preload() {
     // debugger
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.load.spritesheet('player', 'assets/sprites/MC_Link.gif', 16, 16)
   }
 
   function create() {
     const TileMapper = myGame.TileMapper.TileMapper();
     TileMapper.bar()
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    var playerGroup = game.add.group();
+    player = new Player(game);
+    playerGroup.add(player);
+    cursors = game.input.keyboard.createCursorKeys();
   }
 
   function update() {
+    player.update_movement(cursors);
   }
 
   function render() {
---
+++ src/js/player.js
@@ -0,0 +1,35 @@
+Player = function(game) {
+  var x = 100;
+  var y = 100;
+  this.speed = 96;
+  Phaser.Sprite.call(this, game, x, y, 'player', 28);
+  game.physics.arcade.enable(this);
+};
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update_movement = function(cursors){
+
+    this.body.velocity.x = 0;
+    this.body.velocity.y = 0;
+
+    if (cursors.left.isDown)
+    {
+        this.body.velocity.x = -this.speed;
+    }
+    else if (cursors.right.isDown)
+    {
+        this.body.velocity.x = this.speed;
+    }
+
+    if (cursors.up.isDown)
+    {
+        this.body.velocity.y = -this.speed;
+    }
+    else if (cursors.down.isDown)
+    {
+        this.body.velocity.y = this.speed;
+    }
+
+};
