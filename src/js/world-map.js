93423d9f7ba8edb409fedd7e1052fcee01822e1a
Tyler Parker <btylerparker@gmail.com>
2017-04-15 18:02:25 -0400

    World map

---
+++ src/js/world-map.js
@@ -0,0 +1,17 @@
+window.myGame = window.myGame || {};
+
+(function(Phaser, myGame) {
+  const WorldMap = function(game, tileset, tiles, tilemap) {
+    this.map = game.add.tilemap(tilemap);
+    this.map.addTilesetImage(tileset, tiles);
+    this.backgroundlayer = this.map.createLayer('BackgroundLayer');
+    this.groundLayer = this.map.createLayer('GroundLayer');
+    this.map.setCollisionBetween(1, 100, true, 'GroundLayer');
+    //Change the world size to match the size of this layer
+    this.groundLayer.resizeWorld();
+  };
+
+  WorldMap.prototype.constructor = WorldMap;
+
+  myGame.WorldMap = WorldMap;
+})(window.Phaser, window.myGame);
